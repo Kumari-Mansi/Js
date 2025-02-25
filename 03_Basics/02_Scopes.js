a754caafaf22ed8c3ca63de72493ead6af571f90
Kumari-Mansi <149712258+Kumari-Mansi@users.noreply.github.com>
2025-02-25 15:19:03 +0000

    feat: added extra functionalities of scopes of a function

--- 03_Basics/02_Scopes.js
+++ 03_Basics/02_Scopes.js
@@ -1,3 +1,6 @@
+// ++++++++++++++++++++++++++++++PART-2+++++++++++++++++++++++++++++++++++++
+
+
 // block scope and global scope
 
 // var c = 200
@@ -13,3 +16,43 @@ console.log(a)  //error
 //console.log(c)   // 30 thats why var should be avoided
 
 // { } it is called the scope of the program e.g in if else and functions
+
+
+// ++++++++++++++++++++++++++++++PART-2+++++++++++++++++++++++++++++++++++++
+
+function one(){
+    const username="Madhu"
+
+    function two(){
+        const werbsite = "U-Tube"
+        console.log(username)
+    }
+    //console.log(website)
+    
+    two()
+}
+//one()
+
+
+if(true){
+    const username = "Soni"
+    if(username==="Soni"){
+        const website = " youtube"
+        console.log(username+" studys from " +website);
+        
+    }
+    // console.log(website)  // ReferenceError: website is not defined
+}
+// console.log(username);  // ReferenceError: username is not defined
+
+// +++++++++++++++++++++++++++Intresting++++++++++++++++++++++++++++++++++++++++
+
+console.log(addone(5));
+function addone(num){
+   return num+1 
+}
+
+const addTwo = function(num){ // addTwo is known as expressions
+    return num+2
+}
+console.log(addTwo(5));
