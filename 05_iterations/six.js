5425a838f02778789cf7a0c1b48c067cfc87fd7a
Kumari-Mansi <149712258+Kumari-Mansi@users.noreply.github.com>
2025-03-21 20:12:47 +0000

    Forin loop

---
+++ 05_iterations/six.js
@@ -0,0 +1,64 @@
+const code = ["js","c","Ruby","Python"]
+code.forEach(function (val){
+    console.log(val);
+})
+/*
+js
+c
+Ruby
+Python
+*/
+
+code.forEach((item) => {
+    console.log(item);
+})
+/*
+js
+c
+Ruby
+Python
+*/
+
+function print(item){
+    console.log(item);
+}
+code.forEach(print)
+/*
+js
+c
+Ruby
+Python
+*/
+
+code.forEach((item,index,arr)=>{
+    console.log(item,index,arr);
+})
+/*
+js 0 [ 'js', 'c', 'Ruby', 'Python' ]
+c 1 [ 'js', 'c', 'Ruby', 'Python' ]
+Ruby 2 [ 'js', 'c', 'Ruby', 'Python' ]
+Python 3 [ 'js', 'c', 'Ruby', 'Python' ]
+*/
+
+const mycode = [
+    {
+        languageName:"Javascript",
+        languageFileName:"js"
+    },
+    {
+        languageName:"Java",
+        languageFileName:"java"
+    },
+    {
+        languageName:"Python",
+        languageFileName:"py"
+    }
+]
+
+myCoding.forEach((item)=>
+{
+    console.log(item);
+    
+})
+myCoding.forEach(printME)
+
