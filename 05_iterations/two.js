dde201040617e73d24fca7b15c130a34964b5b0e
Kumari-Mansi <149712258+Kumari-Mansi@users.noreply.github.com>
2025-03-21 18:19:50 +0000

    While loop concept

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// While loop
+
+// initialisation
+// while (condition) {
+// Statements
+// increment/decrement
+// }
+
+let index = 0
+while (index<=10) {
+    console.log(`Value of index is : ${index}`);
+    index=index+2;
+}
+
+let arr = ['Raushan','Rahul','Ramesh']
+let i = 0;
+while (i<arr.length) {
+    console.log(arr[i]);
+    i++
+}
