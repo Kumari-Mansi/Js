d8a61b59ca1260916c12c14a8a4684018cf37810
Kumari-Mansi <149712258+Kumari-Mansi@users.noreply.github.com>
2025-04-25 11:38:13 +0000

    Async JavaScript Fundamentals

---
+++ 08_Events/async.js
@@ -0,0 +1,16 @@
+/*
+Default Javascript is a :
+--> synchronous
+--> Single threaded
+
+Execution context : Execute one line of a code at a time
+each operation waits for the last one to complete for executing
+
+Blocking and non-blocking code
+
+Blocking Code --> block the flow of program
+read file syn 
+
+Non-Blocking Code --> Do not block execution
+read file asyn 
+*/
