2ea02c9bff69625c52ead91ebbca96efbabcc88c
Kumari-Mansi <149712258+Kumari-Mansi@users.noreply.github.com>
2025-04-27 05:39:27 +0000

    getter and setter with objects

---
+++ 10_OOPS/object_get_set.js
@@ -0,0 +1,27 @@
+const user ={
+    _email:"raja27@yahoo.ac.in",
+    _password:"rajag",
+
+
+get email(){
+    return this._email.toUpperCase()
+},
+
+set email(value){
+    this._email=value
+},
+get password(){
+    return this._password.toUpperCase()
+},
+
+set password(value){
+    this._password=value
+}
+}
+console.log(user.email);
+console.log(user.password);
+
+// const naam = Object.create(user)
+// console.log(naam._email);
+// console.log(naam.password);
+
