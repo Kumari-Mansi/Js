0c1179fe4df24f5fb5f03706fa4d8da113340787
Kumari-Mansi <149712258+Kumari-Mansi@users.noreply.github.com>
2025-04-28 07:18:33 +0000

    comparisons with its outputs

--- 01_Basics/comparisons.js
+++ 01_Basics/comparisons.js
@@ -3,29 +3,60 @@ console.log(2>11)
 console.log(21==1)
 console.log(22>21)
 console.log()
+/*
+true
+false
+false
+true
+*/
 
 
 console.log("2">1)
 console.log("02">1)
+/*
+true
+true*/
 
 console.log()
 console.log(null>=0)
 console.log(null<0)
 console.log(null==0)
 console.log(null!=0)
+/*
+true
+false
+false
+true
+*/
 
 console.log()
 console.log(undefined>=0)
 console.log(undefined<0)
 console.log(undefined==0)
 console.log(undefined!=0)
+/*
+false
+false
+false
+true
+*/
 
 console.log()
 console.log(null>=undefined)
 console.log(null<undefined)
 console.log(null==undefined)
 console.log(null!=undefined)
+console.log(null==0);
+/*
+false
+false
+true
+false
+false
+*/
+
+
 console.log()
 
 // strict check ===
-console.log("2"===2)
+console.log("2"===2)  // false
